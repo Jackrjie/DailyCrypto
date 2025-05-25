f55dd790e1d955bfbfce9fb8d1b5bcfc1c5bf8ee
ren.jie.soh <ren.jie.soh@accenture.com>
2025-05-25 16:53:35 +0800

    update the api url and token

--- app/src/main/java/com/jackrjie/dailycrypto/core/data/networking/HttpClientFactory.kt
+++ app/src/main/java/com/jackrjie/dailycrypto/core/data/networking/HttpClientFactory.kt
@@ -9,6 +9,7 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -29,6 +30,9 @@ object HttpClientFactory {
                 )
             }
             defaultRequest {
+                headers.append(
+                    HttpHeaders.Authorization, "Bearer 2ef2d47bc97b5958dc889e3720b04a9f4ac7f91bf02d43bddab62f7a4cd4d899"
+                )
                 contentType(ContentType.Application.Json)
             }
         }
